d2f4bed88559e0fdc7250122c3aa3f90f3546db0
Zachary G <zachary.gethin@destronia.com>
2023-11-04 01:38:48 +0800

    Add start scripts

---
+++ start.bat
@@ -0,0 +1,5 @@
+@echo off
+:: This command turns off the echoing of commands in the script
+
+:: Run the Discord bot
+python bot.py
---
+++ start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Run the Discord bot
+python bot.py
+
+# Keep the terminal open so you can view the bot's output
+read -p "Press Enter to exit"
